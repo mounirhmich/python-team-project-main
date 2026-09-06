3970636c07dd004ae7754df0b28ca73b5439855e
mounirhmich <152878582+mounirhmich@users.noreply.github.com>
2026-09-06 03:10:22 +0100

    Configure JWT manager for Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,23 @@
-from flask import Flask, app
+from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+
 from app.routes.auth import auth_bp
 
 db = SQLAlchemy()
+jwt = JWTManager()
+
 
 def create_app():
-
     app = Flask(__name__)
-
     app.config.from_object("app.config.config.Config")
 
     db.init_app(app)
+    jwt.init_app(app)
 
     with app.app_context():
         db.create_all()
-    
+
     app.register_blueprint(auth_bp)
 
     return app
